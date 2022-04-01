a8c3a6f53b0789eb0d876da2ad77b1d31ec0ed4e
areebkhan <areebkhan123123@gmail.com>
2022-04-01 15:23:56 +0500

    added seed-data

---
+++ backend/config/seed-data.js
@@ -0,0 +1,31 @@
+const user_repo = require('../repository/user_repo');
+const uuid = require('uuid').v1;
+
+const user = {
+    FirstName: "Admin",
+    LastName: "User",
+    EmployeeID: "Admin01",
+    Login_ID: "admin01",
+    Email: "admin",
+    RoleTitle: "Admin",
+    CreationDetails: {
+        ActionTakenByName: "System",
+        ActionTakenOn: new Date(),
+    },
+    IsMaster : true,
+    Password: uuid().substr(0, 6),
+};
+
+
+user_repo.find({'Login_ID':'admin01'}, true).then(user_exist => {
+    if(!user_exist){ 
+        let pwd = user.Password;
+        user_repo.create(user).then(user_new => {
+            console.log(`User Created. Please note the password: ${pwd}`);
+        });
+    }
+    else{
+        console.log(`User Already Exist in this Organization:`);
+    }
+});
+ 
