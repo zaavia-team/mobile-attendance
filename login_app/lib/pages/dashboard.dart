4cbe6d75ad02ab264d88ca81b47d8adac70c75c5
Sarmad Akhlaq <sarmadakhlaq.66@gmail.com>
2022-04-06 11:12:37 +0500

    done with sign in/sign out,toast messages and UI

--- login_app/lib/pages/dashboard.dart
+++ login_app/lib/pages/dashboard.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:hive/hive.dart';
+import 'package:http/http.dart' as http;
 
 class Dashboard extends StatefulWidget {
   @override
@@ -10,15 +14,115 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   final iAmInController = TextEditingController();
   final iAmOutController = TextEditingController();
-  var transactionType = "";
+  var transactionType = 'i am In';
+
+  var token;
+
+  late Box box1;
+
+  @override
+  void initState() {
+    super.initState();
+    createBox();
+  }
 
   DateTime? _dateTime;
+  bool button = false;
+
   String getDate() {
     if (_dateTime == null) {
       return 'Select Date';
     } else {
       return DateFormat('dd/MM/yyyy HH:mm').format(_dateTime!);
     }
+  }
+
+  void createBox() async {
+    box1 = await Hive.openBox('loginData');
+    getData();
+  }
+
+  void getData() async {
+    if (box1.get('token') != null) {
+      token = box1.get('token');
+    }
+  }
+
+  void attendanceDetails(BuildContext context) async {
+    var response = await http.post(
+        Uri.parse("http://192.168.18.51:3000/api/attendance_transaction"),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': token
+        },
+        body: jsonEncode({
+          'TransactionType': transactionType,
+          'Date':
+              _dateTime?.toIso8601String() ?? DateTime.now().toIso8601String()
+        }));
+
+    if (transactionType == 'i am Out') {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text("Are you sure your want to sign out",
+                    textAlign: TextAlign.center),
+                actions: [
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        RaisedButton(
+                          onPressed: () {
+                            setState(() {
+                              transactionType = 'i am In';
+                            });
+                            Fluttertoast.showToast(
+                              msg: jsonDecode(response.body)["message"],
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 2,
+                              backgroundColor: Colors.purple,
+                              fontSize: 15
+                            );
+                            Navigator.pop(context);
+                          },
+                          child: Text('Yes'),
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                        ),
+                        RaisedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('No'),
+                          color: Theme.of(context).primaryColor,
+                          textColor: Colors.white,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ));
+    }
+    if (response.statusCode == 200 &&
+        jsonDecode(response.body)["status"] == false) {
+      Fluttertoast.showToast(
+          msg: jsonDecode(response.body)["message"],
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 2,
+          backgroundColor: Colors.purple,
+          fontSize: 15);
+    } 
+    else {
+      setState(() {
+        transactionType = "i am Out";
+            // transactionType == 'i am Out' ? "i am In" : 'i am Out';
+      });
+      
+    }
+    print(response.body);
   }
 
   Future pickDateTime(BuildContext context) async {
@@ -67,62 +171,50 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dashboard"),
+        automaticallyImplyLeading: false,
+        title: const Text("Main Screen"),
       ),
-        body: Card(
-          elevation: 5,
-          child: Center(
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    _dateTime == null
-                        ? '${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}, ${DateTime.now().hour}:${DateTime.now().minute}'
-                        : getDate(),
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      pickDateTime(context);
-                    },
-                    child: Text('Pick a date', style: TextStyle(fontSize: 15)),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      RaisedButton(
-                        onPressed: () {
-                          // TransactionType = "In";
-                          // () => addAttendance;
-                          var box = Hive.box('attendance');
-                          box.put('In', 'In');
-                        },
-                        child: const Text('I am in'),
-                        color: Colors.purple,
-                        textColor: Colors.white,
-                      ),
-                      RaisedButton(
-                        onPressed: () {
-                          // TransactionType = "Out";
-                          var box = Hive.box('attendance');
-                          String data = box.get('In');
-                        },
-                        child: const Text('I am out'),
-                        color: Colors.purple,
-                        textColor: Colors.white,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+      body: Card(
+        elevation: 5,
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  _dateTime == null
+                      ? '${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}, ${DateTime.now().hour}:${DateTime.now().minute}'
+                      : getDate(),
+                  style: TextStyle(fontSize: 20),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    pickDateTime(context);
+                  },
+                  child: Text('Pick a date', style: TextStyle(fontSize: 15)),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    RaisedButton(
+                      child: Text(transactionType),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        attendanceDetails(context);
+                      },
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
+      ),
     );
   }
 }
--- login_app/lib/pages/login.dart
+++ login_app/lib/pages/login.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import './dashboard.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -11,6 +14,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
+  var token;
 
   late Box box1;
 
@@ -18,6 +22,59 @@ class _LoginState extends State<Login> {
   void initState(){
     super.initState();
     createBox();
+  }
+
+  void login() async {
+
+    if (email.text.isEmpty || password.text.isEmpty) {
+        Fluttertoast.showToast(
+      msg: "Kindly fill both the fields",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 2,
+      backgroundColor: Colors.purple,
+      fontSize: 15
+    );
+    } 
+    else{
+    try {
+    var response = await http.post(Uri.parse("http://192.168.18.51:3000/api/login"),
+    headers: <String, String>{
+    'Content-Type':
+        'application/json',
+  },
+    body: jsonEncode({
+      "Login_ID": email.text,
+      "Password": password.text }
+    ));
+
+    if(response.statusCode == 200 && jsonDecode(response.body)["status"] == false){
+    Fluttertoast.showToast(
+      msg: "User ID or Password does not match",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 2,
+      backgroundColor: Colors.purple,
+      fontSize: 15
+    );
+    } else{
+      var data = jsonDecode(response.body);
+      box1.put('email', email.value.text);
+      box1.put('password', password.value.text);
+      box1.put('token', data["token"]);
+      Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Dashboard()),
+      );
+    }
+
+    print(response.body);
+    } catch (e) {
+      print(e);
+    }
+
+  }
+
   }
 
   void createBox() async {
@@ -32,70 +89,74 @@ class _LoginState extends State<Login> {
     if(box1.get('password')!=null){
       password.text = box1.get('password');
     }
-  }
-
-  void login() {
-    box1.put('email', email.value.text);
-    box1.put('password', password.value.text);
-
-    const snackBar = SnackBar(
-      content: Text('Kindly fill both the fields'),
-    );
-
-    if (email.text.isEmpty || password.text.isEmpty) {
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        
-    } else{
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => Dashboard()),
-      );
+    if(box1.get('token')!=null){
+      token = box1.get('token');
     }
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Zaavia'),
+        title: const Text('Attendance App'),
         backgroundColor: Colors.purple,
       ),
       body: Card(
         child: Container(
-          padding: EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextField(
                 keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                     hintText: 'Email',
                     prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder()),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0)
+                      )
+                    )
+                  ),
                 controller: email,
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               TextField(
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: 'Password',
                   prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(),
+                   border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 2),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0)
+                      )
+                    )
                 ),
                 controller: password,
                 obscureText: true,
               ),
-              SizedBox(
-                height: 5,
+              const SizedBox(
+                height: 8,
               ),
-              RaisedButton(
-                onPressed: () {
-                  login();
-                },
-                child: Text('Log in'),
-                color: Colors.purple,
-                textColor: Colors.white,
+              Container(
+                child: RaisedButton(
+                  elevation: 5,
+                  onPressed: () {
+                    login();
+                  },
+                  child: const Text('Log in',
+                    style: TextStyle(
+                      fontSize: 15
+                    ),
+                  ),
+                  color: Colors.purple,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                ),
               )
             ],
           ),
