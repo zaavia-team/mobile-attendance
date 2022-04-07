e7ea16d1814cc9bf40fb41bad704e3a2464db113
sharjeel khan <Ksharjeel007@gmail.com>
2022-04-07 14:25:27 +0500

    aggregation pipeline Report

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -10,6 +10,7 @@ routes.get('/users', controller.getUsers);
 routes.post('/register',  auth.authMiddleware,controller.register);
 routes.post('/attendance_transaction',auth.authMiddleware,attendance_controller.attendance)
 routes.get('/gettodayattendance',auth.authMiddleware,attendance_controller.gettodayattendance)
+routes.post('/getreportattendance',auth.authMiddleware,attendance_controller.report)
 
 
 
