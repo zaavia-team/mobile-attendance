02b3e443fd7783a5479d7214990b73543afbfc76
sharjeel khan <Ksharjeel007@gmail.com>
2022-04-07 15:33:50 +0500

    routes update

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,7 +7,7 @@ let auth = require('../services/auth');
 
 routes.post('/login', controller.login);
 routes.get('/users', controller.getUsers);
-routes.put('/user/:id', auth.authMiddleware, controller.editUser);
+routes.post('/user/:id', auth.authMiddleware, controller.editUser);
 routes.post('/register',  auth.authMiddleware,controller.register);
 routes.post('/attendance_transaction',auth.authMiddleware,attendance_controller.attendance)
 routes.get('/gettodayattendance',auth.authMiddleware,attendance_controller.gettodayattendance)
