31661435dbfad40033869f5be305d6d7d6471211
Sarmad Akhlaq <sarmadakhlaq.66@gmail.com>
2022-04-08 14:20:07 +0500

    added signIn/signOut and logout functionality.. also rendered logged in users list

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:login_app/pages/login.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async {
+Future <void> main() async {
+  await dotenv.load();
   await Hive.initFlutter();
   
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
